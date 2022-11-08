57c98f7a1a76b56f6b3ccb78fab20e85d5f13d17
xMathematics <dongzexu@hotmail.com>
2022-11-08 20:14:12 +0800

    update

--- src/Header/GameApp.h
+++ src/Header/GameApp.h
@@ -5,4 +5,17 @@
 #ifndef DIRECTXTUTORIAL_GAMEAPP_H
 #define DIRECTXTUTORIAL_GAMEAPP_H
 
+#include "d3dApp.h"
+class GameApp : public D3DApp
+{
+public:
+    GameApp(HINSTANCE hInstance, const std::wstring& windowName, int initWidth, int initHeight);
+    ~GameApp();
+
+    bool Init();
+    void OnResize();
+    void UpdateScene(float dt);
+    void DrawScene();
+};
+
 #endif //DIRECTXTUTORIAL_GAMEAPP_H
